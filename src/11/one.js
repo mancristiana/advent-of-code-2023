a904067ceb314f8442201db6a7c229a622b365a3
Cristiana Man <man.cristiana1@gmail.com>
2023-12-11 14:47:30 +0100

    day 11

---
+++ src/11/one.js
@@ -0,0 +1,59 @@
+import { readInput } from "../utils/readInput.js";
+
+const getGalaxies = (matrix) =>
+  matrix.reduce((galaxies, row, y) => {
+    row.forEach((element, x) => {
+      if (element === "#") {
+        galaxies.push({ x, y });
+      }
+    });
+    return galaxies;
+  }, []);
+
+const getEmptyRows = (matrix) =>
+  matrix.reduce((rows, row, y) => {
+    if (row.every((element) => element === ".")) {
+      rows.push(y);
+    }
+    return rows;
+  }, []);
+
+const getEmptyCols = (matrix) =>
+  Array.from({ length: matrix[0].length }, (_, i) => i).filter((i) =>
+    matrix.every((row) => row[i] === "."),
+  );
+
+const solve = (data) => {
+  const matrix = data
+    .split("\n")
+    .slice(0, -1)
+    .map((row) => row.split(""));
+
+  const galaxies = getGalaxies(matrix);
+  const emptyRows = getEmptyRows(matrix);
+  const emptyCols = getEmptyCols(matrix);
+
+  const expandedGalaxies = galaxies.map(({ x, y }) => ({
+    x: x + emptyCols.filter((col) => col < x).length,
+    y: y + emptyRows.filter((row) => row < y).length,
+  }));
+
+  const galaxyPairs = expandedGalaxies.reduce(
+    (pairs, galaxyA, i) => [
+      ...pairs,
+      ...expandedGalaxies.slice(i + 1).map((galaxyB) => ({ galaxyA, galaxyB })),
+    ],
+    [],
+  );
+
+  return galaxyPairs.reduce((sum, { galaxyA, galaxyB }) => {
+    const distance =
+      Math.abs(galaxyA.x - galaxyB.x) + Math.abs(galaxyA.y - galaxyB.y);
+    return sum + distance;
+  }, 0);
+};
+const data = await readInput();
+
+const result = solve(data);
+
+console.log(result);
